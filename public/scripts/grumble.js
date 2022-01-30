87cbd76228882347a78104afaf26b0a278088211
JelNiSław <jelnislaw@gmail.com>
2022-01-30 16:16:29 +0100

    Disable request cache

--- public/scripts/grumble.js
+++ public/scripts/grumble.js
@@ -1,6 +1,6 @@
 addEventListener("load", () => {
   const grumble = document.getElementById("grumble");
-  fetch("https://grumble.jelnislaw.workers.dev/").then(
+  fetch("https://grumble.jelnislaw.workers.dev/", { cache: "no-store" }).then(
     async response => {
       const json = await response.json();
       grumble.textContent = json.grumble;
